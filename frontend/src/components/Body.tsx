2ff9c5d30c27b262ac559b28d62b01e83502e968
Lucca Stern Bianchi <lucca.bianchi@aluno.ifsp.edu.br>
2026-03-11 13:33:58 -0300

    Adicionado todos os projetos. Fix me CORRIGIDO

--- frontend/src/components/Body.tsx
+++ frontend/src/components/Body.tsx
@@ -26,10 +26,46 @@ function Body() {
 
                 <Projeto 
                     nome="Oscars" 
-                    urlImg="https://github.com/SternLucca/Oscars/blob/main/oscar-icon.png?raw=true" 
+                    urlImg="https://raw.githubusercontent.com/SternLucca/Oscars/refs/heads/main/oscar-icon.png" 
                     urlRepo="https://github.com/SternLucca/Oscars/" 
                     urlSite={undefined} />
+
+                <Projeto 
+                    nome="Pedra Papel Tesoura" 
+                    urlImg="https://raw.githubusercontent.com/SternLucca/RockPaperScissors/refs/heads/main/rock.png" 
+                    urlRepo="https://github.com/SternLucca/RockPaperScissors" 
+                    urlSite="https://sternlucca.github.io/RockPaperScissors/" />
+
+                <Projeto 
+                    nome="Mario" 
+                    urlImg="https://raw.githubusercontent.com/SternLucca/Mario/refs/heads/main/Imagens/mario-icon.ico" 
+                    urlRepo="https://github.com/SternLucca/Mario" 
+                    urlSite="https://sternlucca.github.io/Mario/HTML/index.html" />
+
+                <Projeto 
+                    nome="Extensão" 
+                    urlImg="https://raw.githubusercontent.com/SternLucca/Extension/refs/heads/main/icon.png" 
+                    urlRepo="https://github.com/SternLucca/Extension/" 
+                    urlSite={undefined} />
+
+                <Projeto 
+                    nome="Pixel-Art" 
+                    urlImg="https://raw.githubusercontent.com/SternLucca/Pixel-Art/refs/heads/main/pincel.ico" 
+                    urlRepo="https://github.com/SternLucca/Pixel-Art" 
+                    urlSite="https://sternlucca.github.io/Pixel-Art/" />
                 
+                <Projeto 
+                    nome="Sombreropedia" 
+                    urlImg="https://raw.githubusercontent.com/SternLucca/Sombreropedia/refs/heads/main/Imagens/icon.png" 
+                    urlRepo="https://github.com/SternLucca/Sombreropedia" 
+                    urlSite="https://sternlucca.github.io/Sombreropedia/" />
+
+                <Projeto 
+                    nome="Banana" 
+                    urlImg="https://raw.githubusercontent.com/SternLucca/Banana/refs/heads/main/banana.png" 
+                    urlRepo="https://github.com/SternLucca/Banana/" 
+                    urlSite="https://sternlucca.github.io/Banana/" />
+
             </div>
         </section>
     )
